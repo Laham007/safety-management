1a2f4057ec18edd34b22bb39cff609f13fd67657
unknown <your-email@example.com>
2025-05-22 20:53:09 +0300

    fix: update configuration for static export

--- next.config.mjs
+++ next.config.mjs
@@ -1,19 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
-  swcMinify: true,
+  output: 'export',
+  distDir: 'out',
   images: {
     unoptimized: true,
-    domains: ['safety-management.netlify.app'],
   },
-  experimental: {
-    appDir: true,
-  },
-  output: 'standalone',
   trailingSlash: true,
+  basePath: '',
+  assetPrefix: '.',
 };
 
-// For Netlify deployment
-const withTM = require('next-transpile-modules')([]);
-
-module.exports = withTM(nextConfig);
+module.exports = nextConfig;
